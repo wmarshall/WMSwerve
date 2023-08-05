8fc889a7cc87c19d91c6b46e3f23c1d74aa104ee
Will Marshall <willcodymarshall@gmail.com>
2023-08-05 12:29:00 -0400

    use the relative encoder for driving and the absolute encoder for seeding

--- src/main/java/frc/robot/subsystems/SwerveDriveTrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveTrain.java
@@ -7,13 +7,22 @@ import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 import frc.robot.Util;
-
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class SwerveDriveTrain implements Subsystem {
 
     public static double WHEEL_BASE_INCHES = 18;
     public static double TRACK_WIDTH_INCHES = 24;
+
+    // From https://www.revrobotics.com/rev-21-3005/
+    // Reductions are always measured as turns-in/turns-out
+    public static double STEERING_MOTOR_REDUCTION = 9424.0 / 203.0;
+    public static double DRIVE_MOTOR_PINION_TEETH = 12;
+    // Spot check - with a 12 tooth pinion this is ~5.5:1, as stated by the docs
+    public static double DRIVE_MOTOR_REDUCTION = (22.0 / DRIVE_MOTOR_PINION_TEETH) * (45.0 / 15.0);
+    public static double DRIVE_WHEEL_DIAMETER_INCHES = 3;
+    public static double DRIVE_WHEEL_CIRCUMFRENCE_INCHES = DRIVE_WHEEL_DIAMETER_INCHES * Math.PI;
 
     public static double L = WHEEL_BASE_INCHES / 2;
     public static double W = TRACK_WIDTH_INCHES / 2;
@@ -26,43 +35,144 @@ public class SwerveDriveTrain implements Subsystem {
 
     private double fwd, str, rcw;
 
-    private CANSparkMax steerFactory(int deviceID) {
-        var steer = new CANSparkMax(deviceID, MotorType.kBrushless);
-        // Assume NEO550
-        steer.setSmartCurrentLimit(30);
-        var heading_encoder = steer.getAbsoluteEncoder(Type.kDutyCycle);
-        var motor_encoder = steer.getEncoder();
-        // Heading in degrees, 0 is "north"
-        Util.handleREVLibErr(heading_encoder.setPositionConversionFactor(360));
-        Util.handleREVLibErr(motor_encoder.setPositionConversionFactor(360));
+    private CANSparkMax driveFactory(int deviceID, boolean motorInverted) {
+        var drive = new CANSparkMax(deviceID, MotorType.kBrushless);
+        Util.handleREVLibErr(drive.restoreFactoryDefaults());
+        drive.setInverted(motorInverted);
 
-        // TODO - persistent offsets
-        Util.handleREVLibErr(heading_encoder.setZeroOffset(0));
-        Util.handleREVLibErr(motor_encoder.setPosition(heading_encoder.getPosition()));
+        // native units are Rotations, RPM
+        // we want Inches/ inches per second
+        var encoder = drive.getEncoder();
+        Util.handleREVLibErr(
+                encoder.setPositionConversionFactor(DRIVE_WHEEL_CIRCUMFRENCE_INCHES / DRIVE_MOTOR_REDUCTION));
+        Util.handleREVLibErr(encoder.setVelocityConversionFactor(encoder.getPositionConversionFactor() / 60));
+
+        return drive;
+    }
+
+    private CANSparkMax steerFactory(int deviceID, double zeroOffset, boolean headingEncoderInverted,
+            boolean relativeEncoderInverted, boolean motorInverted) {
+        var steer = new CANSparkMax(deviceID, MotorType.kBrushless);
+        Util.handleREVLibErr(steer.restoreFactoryDefaults());
+
+        steer.setInverted(motorInverted);
+        // Assume NEO550, don't smoke the poor motor
+        steer.setSmartCurrentLimit(30);
+
+        // The duty cycle encoder's native units are [0, 1) rotations
+        // We want to scale to degrees, then apply the known degree offset, then seed
+        // the relative encoder.
+        var heading_encoder = steer.getAbsoluteEncoder(Type.kDutyCycle);
+        // Rotations -> degrees, RPM -> degrees per second
+        Util.handleREVLibErr(heading_encoder.setPositionConversionFactor(1 / 360.0));
+        Util.handleREVLibErr(
+                heading_encoder.setVelocityConversionFactor(heading_encoder.getPositionConversionFactor() / 60));
+        Util.handleREVLibErr(heading_encoder.setInverted(headingEncoderInverted));
+
+        // Each module has its own zero offset:
+        // if we're passed a zero offset of -90, wrap that to 270.
+        // if passed an offset of 450, wrap to +90
+        var wrappedZeroOffset = MathUtil.inputModulus(zeroOffset, 0, 360);
+        Util.handleREVLibErr(heading_encoder.setZeroOffset(wrappedZeroOffset));
+
+        // The duty cycle encoder now reports the heading of the module in range [0,
+        // 360)
+        // inputModulus allows us to convert that to [-180, 180), with 0 still as
+        // "north"
+        var true_heading = MathUtil.inputModulus(heading_encoder.getPosition(), -180, 180);
+
+        // We use the absolute encoder for seeding but the relative encoder for control:
+        // - this allows us to survive the absolute encoder becoming unplugged during
+        // the match,
+        // - as well as have higher control resolution (1024 CPR on the absolute
+        // encoder, 42*STEERING_MOTOR_REDUCTION ~=1949 CPR for the relative)
+        // - the relative encoder is able to report negative readings w/o us needing to
+        // do any trickery here
+        // - This approach may introduce backlash concerns, but we'll leave that for
+        // experimental determination
+        var motor_encoder = steer.getEncoder();
+        motor_encoder.setInverted(relativeEncoderInverted);
+        // Rotations -> degrees, RPM -> degrees per second
+        Util.handleREVLibErr(motor_encoder.setPositionConversionFactor(1 / (360.0 * STEERING_MOTOR_REDUCTION)));
+        Util.handleREVLibErr(
+                motor_encoder.setPositionConversionFactor(motor_encoder.getPositionConversionFactor() / 60));
+
+        // Seed the relative encoder with the true heading
+        Util.handleREVLibErr(motor_encoder.setPosition(true_heading));
 
         // Configire PID
         var controller = steer.getPIDController();
-        Util.handleREVLibErr(controller.setFeedbackDevice(heading_encoder));
+        Util.handleREVLibErr(controller.setFeedbackDevice(motor_encoder));
         Util.handleREVLibErr(controller.setPositionPIDWrappingEnabled(true));
         Util.handleREVLibErr(controller.setPositionPIDWrappingMinInput(-180));
         Util.handleREVLibErr(controller.setPositionPIDWrappingMaxInput(180));
-        // In units of output fraction (-1, 1) per degree of error
+        // In units of output fraction [-1, 1] per degree of error
         Util.handleREVLibErr(controller.setP(0.05));
         // Enable the controller
-        Util.handleREVLibErr(controller.setReference(0, ControlType.kPosition));
+        Util.handleREVLibErr(controller.setReference(motor_encoder.getPosition(), ControlType.kPosition));
 
         return steer;
     }
 
     public SwerveDriveTrain() {
-        FLDrive = new CANSparkMax(0, MotorType.kBrushless);
-        FLSteer = steerFactory(1);
-        FRDrive = new CANSparkMax(2, MotorType.kBrushless);
-        FRSteer = steerFactory(3);
-        RLDrive = new CANSparkMax(4, MotorType.kBrushless);
-        RLSteer = steerFactory(5);
-        RRDrive = new CANSparkMax(6, MotorType.kBrushless);
-        RRSteer = steerFactory(7);
+        /*
+         * The zeroing jig provided with MaxSwerve orients each module to:
+         * 
+         * |------------|
+         * |---- ^ |
+         * \ | |
+         * \ | |
+         * \ |
+         * \----|
+         * 
+         * Where the arrow indicates the wheel direction.
+         * When applied to all 4 modules though, this gives:
+         * - FL facing west
+         * - FR facing north
+         * - RL facing south
+         * - RR facing east
+         * 
+         * Our constants for each module are captured from the use of the jig,
+         * then we apply offsets to get them to face north
+         */
+        // TODO: capture from REV Client, move to constants class
+
+        double FLZeroOffsetDegrees = 0;
+        double FRZeroOffsetDegrees = 0;
+        double RLZeroOffsetDegrees = 0;
+        double RRZeroOffsetDegrees = 0;
+
+        FLDrive = driveFactory(0, false);
+        FLSteer = steerFactory(
+                1,
+                FLZeroOffsetDegrees + 90,
+                false,
+                false,
+                false);
+        FRDrive = driveFactory(2,
+                false);
+        FRSteer = steerFactory(
+                3,
+                FRZeroOffsetDegrees + 0,
+                false,
+                false,
+                false);
+        RLDrive = driveFactory(4,
+                false);
+        RLSteer = steerFactory(
+                5,
+                RLZeroOffsetDegrees + 180,
+                false,
+                false,
+                false);
+        RRDrive = driveFactory(6,
+                false);
+        RRSteer = steerFactory(
+                7,
+                RRZeroOffsetDegrees + 270,
+                false,
+                false,
+                false);
     }
 
     // TODO: Should this be implemented as a command factory?
@@ -112,6 +222,8 @@ public class SwerveDriveTrain implements Subsystem {
             rr_speed /= max_output;
         }
 
+        // Use this incredibly long formulation of setting duty cycle so that we can see
+        // errors from the sparkmaxen
         Util.handleREVLibErr(FLDrive.getPIDController().setReference(fl_speed, ControlType.kDutyCycle));
         Util.handleREVLibErr(FRDrive.getPIDController().setReference(fr_speed, ControlType.kDutyCycle));
         Util.handleREVLibErr(RLDrive.getPIDController().setReference(rl_speed, ControlType.kDutyCycle));
@@ -121,23 +233,20 @@ public class SwerveDriveTrain implements Subsystem {
         // snap the angle back to 0 when both inputs are zero. Some people prefer to
         // employ this logic when commanded drive speed is close to zero, I'm of the
         // thought that deadbanding should happen on the input side only, not output.
-        if(fl_speed == 0) {
+        if (fl_speed == 0) {
             fl_angle = FLSteer.getEncoder().getPosition();
         }
-        if(fr_speed == 0) {
+        if (fr_speed == 0) {
             fr_angle = FRSteer.getEncoder().getPosition();
         }
-        if(rl_speed == 0) {
+        if (rl_speed == 0) {
             rl_angle = RLSteer.getEncoder().getPosition();
         }
-        if(rr_speed == 0) {
+        if (rr_speed == 0) {
             rr_angle = RRSteer.getEncoder().getPosition();
         }
 
         // TODO - implement wraparound to minimize wheel direction changes
-
-        // Use this incredibly long formulation of setting duty cycle so that we can see
-        // errors from the sparkmaxen
         Util.handleREVLibErr(FLSteer.getPIDController().setReference(fl_angle, ControlType.kPosition));
         Util.handleREVLibErr(FRSteer.getPIDController().setReference(fr_angle, ControlType.kPosition));
         Util.handleREVLibErr(RLSteer.getPIDController().setReference(rl_angle, ControlType.kPosition));
