08442d51697f609035e25f80f01adb34d601e7c7
Will Marshall <willcodymarshall@gmail.com>
2023-08-07 21:47:50 -0400

    fix ascii art

--- src/main/java/frc/robot/subsystems/SwerveDriveTrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveTrain.java
@@ -96,11 +96,12 @@ public class SwerveDriveTrain implements Subsystem {
         /*
          * The zeroing jig provided with MaxSwerve orients each module to:
          * 
-         * Please don't squish my art VSCode
-         * |--
-         *  ^ |
-         *  | |
-         *  --|
+         * |------------|
+         * |----....^...|
+         * .....\...|...|
+         * ......\..|...|
+         * .......\.....|
+         * ........\----|
          * 
          * Where the arrow indicates the wheel direction.
          * When applied to all 4 modules though, this gives:
